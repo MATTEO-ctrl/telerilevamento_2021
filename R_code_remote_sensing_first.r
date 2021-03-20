c8bc140fad1118776b34d7441e1bb89531cc72fc
MATTEO-ctrl <80513480+MATTEO-ctrl@users.noreply.github.com>
2021-03-20 18:05:19 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -6,3 +6,7 @@ library(raster)
 p224r63_2011 <-brick("p224r63_2011_masked.grd")
 p224r63_2011 
 plot(p224r63_2011)
+# a vector of colours
+cl <- colorRampPalette(c("blue","orange","green")) (100)
+#il primo argomento della funzione è l'immagine che vogliamo plottare!
+plot(p224r63_2011,col=cl)
