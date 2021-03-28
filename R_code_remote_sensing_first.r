98e3de080c87caaaa2d7fa0e9d588993098139b3
MATTEO-ctrl <80513480+MATTEO-ctrl@users.noreply.github.com>
2021-03-28 17:08:01 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -63,5 +63,21 @@ clr <- colorRampPalette(c("dark red","red","pink")) (100)
 plot(p224r63_2011$B3_sre, col=clr)
 clnir <- colorRampPalette(c("red","orange","yellow")) (100)
 plot(p224r63_2011$B4_sre, col=clnir)
-
+# Day 4 visualizing data by RGB plotting
+library(raster)
+setwd("C:/lab/") # Windows
+p224r63_2011 <-brick("p224r63_2011_masked.grd")
+# Bande Landsat
+# B1: blu
+# B2: green
+# B3: red
+# B4: near infrared
+# B5: middle infrared
+# B6: thermal infrared
+# B7: middle infrared
+# Make a Red-Green-Blue plot based on three layers
+# Apply linear stretch
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+# the vegetation in the infrared has a very high reflectance I associate the band number 4 to the component Red 
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 
