b91a153ce57af856ce3c48e54261e3c68e7581b4
MATTEO-ctrl <80513480+MATTEO-ctrl@users.noreply.github.com>
2021-03-28 21:18:49 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -80,4 +80,43 @@ p224r63_2011 <-brick("p224r63_2011_masked.grd")
 plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
 # the vegetation in the infrared has a very high reflectance I associate the band number 4 to the component Red 
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+# you can notice well the agricultural land(violet)
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+# infrared in the blue component
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+# Mount a 2X2 multiframe 
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+dev.off()
+# an image with natural colors has less definition than infrared
+par(mfrow=c(2,2))
+ plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+> 
+> plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+> 
+> plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+> 
+> plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+# How to make a pdf
+pdf("my_first_pdf.pdf")
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+dev.off()
+# not linear stretch
+# with histogram stretch have more definition inside the forest to analyze for example the rates of humidity.
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Hist")
+# par natural colors, false colour and false colours with histogram stretch
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Hist")
+
+
 
