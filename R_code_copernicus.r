c9aa7a4e9aed9e18295dd13c92c57242031ef5a9
MATTEO-ctrl <80513480+MATTEO-ctrl@users.noreply.github.com>
2021-04-20 15:11:45 +0200

    Create R_code_copernicus.r

---
+++ R_code_copernicus.r
@@ -0,0 +1,6 @@
+# R_code_copernicus.r
+# Copernicus global land service che fornire prodotti bio-geofisici della superficie terrestre globale.
+library(raster)
+# install.packages("ncdf4")
+library(ncdf4)
+setwd("/lab/")
