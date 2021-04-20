b9970a58b8ad2d5ca8b50f391382f2221185587f
MATTEO-ctrl <80513480+MATTEO-ctrl@users.noreply.github.com>
2021-04-20 15:39:55 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -3,4 +3,7 @@
 library(raster)
 # install.packages("ncdf4")
 library(ncdf4)
-setwd("/lab/")
+setwd("C:/lab/")
+# nome del mio dataset
+Lake water quality <- raster("c_gls_LWQ300_202104010000_GLOBE_OLCI_V1.4.0.nc")
+# digitando su R Lake water quality vediamo le caratteristiche quali classe, dimensione, sistema di riferimento.
