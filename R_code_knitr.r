2d3b149417c5849463d4c1a7526a73330980d3c3
MATTEO-ctrl <80513480+MATTEO-ctrl@users.noreply.github.com>
2021-04-20 18:47:34 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -2,3 +2,4 @@
 # Combina molte caratteristiche in un unico pacchetto
 setwd("C:/lab/") 
 library(knitr)
+stitch("R_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
