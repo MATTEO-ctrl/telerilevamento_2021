73b19c7ced7b4a4180a8d4db092d1f00baa9bb1e
MATTEO-ctrl <80513480+MATTEO-ctrl@users.noreply.github.com>
2021-04-27 12:12:29 +0200

    Update R_code_classificazione.r

--- R_code_classificazione.r
+++ R_code_classificazione.r
@@ -1,8 +1,9 @@
 # R_code_classificazione.r
-# scaricato un immagine che rappresenta i diversi livelli energetici di una parte del sole
-setwd("C:/lab/")
-# usiamo la funzione brick che utilizza un pacchetto di dati e li unisce 
+# scaricato un immagine che rappresenta i diversi livelli energetici di una parte del sole 
 library(raster)
+library(RStoolbox)
+setwd("C:/lab/")
+# usiamo la funzione brick che utilizza un pacchetto di dati e li unisce
 solar <-brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 # lavoriamo con bande già elaborate
 #visualizziamo i livelli RGB 
@@ -23,3 +24,14 @@ SUN <-brick("The Sun.png")
 sunc <- unsuperClass(SUN , nClasses=3)
 plot(sunc$map)
 # abbiamo un immagine con i livelli ben definiti in questo caso
+# Immagine GRAND CANYON composto da scisti nella parte basale e rocce vulcaniche nella parte superiore. La mineralogia che è all'interno di queste rocce determinerà la riflettanza.
+# usando la riflettanza possiamo classificare la nostra immagine che è un RGB quindi formata da tre livelli.
+gc <-brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+# plottiamo l'immagine con la seguente funzione sia con stretch linear sia stretch hist per vedere le differenze in particolare con hist si nota meglio la tridimensionalità
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+gcc2 <-unsuperClass(gc, nClasses=2)
+plot(gcc2$map)
+# si dissociano molto bene le due classi date da due litologie differenti
+gcc4 <-unsuperClass(gc, nClasses=4)
+plot(gcc4$map)
