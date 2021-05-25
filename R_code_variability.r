22ea98c27d2a07fa007bed5cf7f73360e1f86121
MATTEO-ctrl <80513480+MATTEO-ctrl@users.noreply.github.com>
2021-05-25 12:40:27 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -35,8 +35,41 @@ plot(sentpca$map)
 summary(sentpca$model)
 # la prima PC spiega il 67.3% della variabilità originale.
 
-
+# utilizziamo la prima componente associando tutto ad un oggetto per poi utilizzare la funzione 'focal' e vedere come cambiano i valori su una singola banda.
+pc1 <- sentpca$map$PC1
+pc1sd5 <- focal(pc1, w=matrix(1/25, nrow=5, ncol=5), fun=sd)
+clpc <- colorRampPalette(c('blue', 'green', 'pink', 'magenta', 'orange', 'brown', 'red', 'yellow')) (100)
+plot(pc1sd5, col=clpc)
+# si nota una grande variabilità geomorfologica. Le praterie di alta quota sono le zone più omogenee.
+# La funzione 'source' permette di utilizzare un parte di codice che è fuori R e caricarlo direttamente dentro R.
+source("source.test.lezione.r")
+# codice all'esterno
+pc1 <- sentpca$map$PC1
+pc1sd7 <- focal(pc1, w=matrix(1/49, nrow=7, ncol=7), fun=sd)
+plot(pc1sd7)
 library(ggplot2)
-# install.packages("viridis")
-library(viridis)
 library(gridExtra)
+# install.packages("viridis") serve per colorare i plot in modo automatico. 
+library(viridis)
+source("source_ggplot.r")
+ggplot() +
+geom_raster(pc1sd5, mapping= aes( x=x, y=y, fill = layer))
+# Possiamo individuare le varie discontinuità geogorafiche, le caratteristiche geomorfologiche della zona, e i vari ecotoni.
+p1 <- ggplot() +
+geom_raster(pc1sd5, mapping= aes( x=x, y=y, fill = layer)) +
+scale_fill_viridis() +
+ggtitle("Standard deviation of PC1 by Viridis colour scale")
+# funzione "magma" le zone con alta deviazione standard si vedono molto chiaramente.
+p2 <- ggplot() +
+geom_raster(pc1sd5, mapping= aes( x=x, y=y, fill = layer)) +
+scale_fill_viridis(option= "magma") +
+ggtitle("Standard deviation of PC1 by magma colour scale")
+# funzione "inferno"
+p3 <- ggplot() +
+geom_raster(pc1sd5, mapping= aes( x=x, y=y, fill = layer)) +
+scale_fill_viridis(option= "turbo") +
+ggtitle("Standard deviation of PC1 by turbo colour scale")
+grid.arrange(p1, p2, p3, nrow=1)
+# "Virdis" e "magma" sono più efficaci rispetto a "turbo" che pone il colore giallo come valori intermedi. Il colore giallo trasmette alla mostra mente che sia il valore massimo.
+
+
